f799283aaf0de42df75cc263b6039aa24b62c87b
Andrii <deyneka2003@gmail.com>
2024-07-11 03:44:50 +0300

    update stuff

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar = async () => {
                   Sign out
                 </Link>
                 {isAdmin ? <Link
-                  href='/api/auth/logout'
+                  href='/dashboard'
                   className={buttonVariants({
                     size: 'sm',
                     variant: 'ghost',
